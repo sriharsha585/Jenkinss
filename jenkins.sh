87556895593500ec92c61c9681f6a922a57015ca
SaiSriHarshaNandam <nandam585@gmail.com>
2024-08-28 17:57:29 +0530

    updated

--- jenkins.sh
+++ jenkins.sh
@@ -4,7 +4,7 @@ pipeline {
     stages {
         stage('Checkout') {
             steps {
-                git branch: 'main', url:  'https://github.com/sriharsha585/Jenkinss'
+                git 'https://github.com/sriharsha585/Jenkinss'
             }
         }
 
